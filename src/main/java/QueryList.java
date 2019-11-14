36c8b4057f60405a63bf8c6e25e6c1425ade4602
Shannazy <shaziislam417@gmail.com>
2019-11-14 14:45:39 -0500

    Added client search

--- src/main/java/QueryList.java
+++ src/main/java/QueryList.java
@@ -56,4 +56,47 @@ public class QueryList {
             return false;
         }
     }
+
+    public boolean searchCLients(String userName, String password){
+        try {
+            String search = "Select Email, Password From Admins WHERE Email = ? and Password = ?";   //Create string for searching admins
+            PreparedStatement stmt = mainConnection.prepareStatement(search);   //create the actual statement
+            stmt.setString(1, userName);    //Adding the first parameter
+            stmt.setString(2, password); //Adding the second parameter
+            ResultSet res = stmt.executeQuery();    //Cursor for my query result
+//            while (res.next()) {    //traverse each row
+//                System.out.println(res.getString(1));   //print the name
+//                System.out.println(res.getString(2));   //print the netid
+//            }
+            if (res.next()) {    //Simple verification that the person exist on our system. Doesn't actually compare names yet
+                String user = res.getString(1);
+                String pass = res.getString(2);
+                if (user.equals(userName)) {  //compare the username found
+                    if (pass.equals(password)) { //Compare the password found
+                        res.close();    //close connections
+                        connector.closeConnection();    //close the actual connection to stop from breaking the database
+                        return true;
+                    } else {
+                        res.close();
+                        connector.closeConnection();
+                        return false;
+                    }
+                } else {
+                    res.close();
+                    connector.closeConnection();
+                    return false;
+                }
+
+            } else {
+                res.close();
+                connector.closeConnection();
+                return false;
+
+            }
+        } catch (Exception e) {
+            connector.closeConnection();
+            return false;
+        }
+
+    }
 }
