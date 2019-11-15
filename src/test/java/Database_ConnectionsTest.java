fec9d00ea50fc9cbba7e0f30d294e923704f2aa8
Shannazy <shaziislam417@gmail.com>
2019-11-15 17:07:04 -0500

    Entire project uploaded

---
+++ src/test/java/Database_ConnectionsTest.java
@@ -0,0 +1,15 @@
+import BackEndCode.Database_Connections;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Database_ConnectionsTest {
+
+    @Test
+    void getConnected() {
+        Database_Connections myConnection = new Database_Connections();
+        assertEquals(true, myConnection.getConnected());
+        myConnection.closeConnection();
+    }
+
+}
