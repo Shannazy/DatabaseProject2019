bb19b698aca0f99d839f76a8ed3a9562c9022880
Shannazy <shaziislam417@gmail.com>
2019-12-10 14:09:51 -0500

    Tested the initial One Way ticket buying method

--- Testing Servlet/src/test/java/BackEndStuff/QueryListTest.java
+++ Testing Servlet/src/test/java/BackEndStuff/QueryListTest.java
@@ -2,6 +2,8 @@ package BackEndStuff;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueryListTest {
@@ -19,5 +21,19 @@ class QueryListTest {
         DatabaseConnection tester = new DatabaseConnection();
         QueryList testingentry = new QueryList(tester);
         assertEquals(true, testingentry.registerClient("BasicEmail.com","Fake Name", "8254176529","2019-11-17","NotRealPass","1998-05-18"));
+    }
+
+
+    @Test
+    void initialOneWayTicket() {
+        DatabaseConnection tester = new DatabaseConnection();
+        QueryList searchForTicket = new QueryList(tester);
+        List<List<String>> myarray = searchForTicket.initialOneWayTicket("2019-12-12","JFK","LAX");
+        for (List<String> part1 : myarray){
+            for (String runner : part1){
+                System.out.print(runner+ " ");
+            }
+            System.out.println();
+        }
     }
 }
